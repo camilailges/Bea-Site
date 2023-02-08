72659e0394749639a249c447da925dfc9176ec66
camilailges <camilailges@gmail.com>
2023-02-07 23:36:16 -0300

    two columms 1st page  correct

--- views/index.php
+++ views/index.php
@@ -1,28 +1,51 @@
-<link rel="stylesheet" href="css/initial.css">
 <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/product/">
+<link rel="stylesheet" href="css/initial.css">
 
 <body>
 <main>
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="position-relative overflow-hidden text-center bg-light">
       <div class="product-device shadow-sm d-none d-md-block"></div>
       <div class="product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
   
     <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-      <div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 py-3">
-          <h2 class="display-5">Another headline</h2>
-          <p class="lead">And an even wittier subheading.</p>
+      <div class="coluna-da-esq">
+        <div class="me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 py-3">
+                <img src="./img/bea.jpg" alt="" style="width: 80%; height: 80%;">
+            <p class="lead">And an even wittier subheading.</p>
+            </div>
         </div>
-        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
       </div>
       <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
           <h2 class="display-5">Another headline</h2>
           <p class="lead">And an even wittier subheading.</p>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
       </div>
     </div>
   </main>
 </body>
+
+<!-- 
+<body>
+<main>
+    <div class="position-relative overflow-hidden text-center bg-light">
+      <div class="product-device shadow-sm d-none d-md-block"></div>
+      <div class="product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+</main>
+<section class="informacoes-gerais">
+    <div class="foto-luciano">
+    <img src="./img/bea.jpg" alt="" style="width: 80%; height: 80%;">
+        <div class="descricao-foto">
+            <h1>Dr. Luciano Ramires</h1>
+        </div>
+    </div>
+    <div class="infos">
+        <h1>Dr. Luciano Ramires</h1>
+        <p>Médico Cirurgião Ortopédico da Sociedade Brasileira de Artroscopia e Traumatologia do Esporte <br />Especialista em Medicina do Esporte pela Sociedade Brasileira de Medicina do Exercício e do Esporte</p>
+        <a href="sobre"><button>Saiba mais sobre a minha trajetória</button></a>
+    </div>
+</section>
+</body> -->
